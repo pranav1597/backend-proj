1eaf5e6292863fe9664b0a76f54cf904ca721cc6
pranav1597 <prnvp355@gmail.com>
2024-03-10 17:48:40 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 
+
 dotenv.config(
     {
         path: './env'
@@ -10,7 +11,21 @@ dotenv.config(
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    });
+
+    app.on("error", (e) => {
+                console.log("ERRR: ", error);
+                throw error
+    })
+})
+.catch(err => {
+    console.log(`Mongodb conn failed `, err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const asyncHandler = (requestHandler) => {
+   (req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+   }
+}
+
+
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+//     }
+//     catch (error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+export default asyncHandler
