0436bfbef1801170b3c0b17607210512ab8a01fb
pranav1597 <prnvp355@gmail.com>
2024-03-30 11:20:41 +0530

    code updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     const { fullName, email, username, password } = req.body
-    console.log("Email: ", email)
+    // console.log("Email: ", email)
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
     )
@@ -34,9 +34,16 @@ const registerUser = asyncHandler( async (req, res) => {
     }
     
     const avatarLocalPath = req.files?.avatar[0]?.path
-    console.log("avatarLocalPath", avatarLocalPath)
-    const coverImagePath = req.files?.coverImage[0]?.path
+    // console.log("avatarLocalPath", avatarLocalPath)
+    // const coverImagePath = req.files?.coverImage[0]?.path
     
+    let coverImagePath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImagePath = req.files.coverImage[0].path
+    }
+
+
+
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
     }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,11 +2,12 @@ import {v2 as cloudinary} from 'cloudinary';
 
 import fs from 'fs'
 
+const {CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET} = process.env
 
 cloudinary.config({ 
-  cloud_name: 'process.env.CLOUDINARY_CLOUD_NAME', 
-  api_key: 'process.env.CLOUDINARY_API_KEY', 
-  api_secret: 'process.env.CLOUDINARY_API_SECRET' 
+  cloud_name: CLOUDINARY_CLOUD_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -19,7 +20,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
 
-        console.log("file is uploaded on cloudinary", response.url)
+        // console.log("file is uploaded on cloudinary", response.url)
+        fs.unlinkSync(localFilePath)
 
         return response
 
