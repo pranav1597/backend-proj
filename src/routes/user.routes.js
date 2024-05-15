d4f26550a43477ba5ad02c2b22324740337cd796
pranav1597 <prnvp355@gmail.com>
2024-05-15 13:40:30 +0530

    fixed errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,8 @@ import {registerUser,
         loginUser,
         logoutUser,
         refreshAccessToken, changeCurrentPassword, getCurrentUser,
-        updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory
+        updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory,
+        updateAccountDetails
         } from "../controllers/user.controller.js";
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,7 +31,7 @@ router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route('/change-password').post(verifyJWT, changeCurrentPassword)
 router.route('/current-user').get(verifyJWT, getCurrentUser)
-router.route('/update-account').patch(verifyJWT, getCurrentUser)
+router.route('/update-account').patch(verifyJWT, updateAccountDetails)
 
 router.route('/avatar').patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route('/cover-image').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
